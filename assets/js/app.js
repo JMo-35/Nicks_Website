b5a42677b89226343945e072b2f697eef4062337
JMo-35 <47646759+JMo-35@users.noreply.github.com>
2022-08-13 17:12:38 -0400

    updates for mobile and css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,22 @@
 const scrollUp = document.getElementById("scroll-up");
 
+const burger = document.getElementById("burger-menu");
+const ul = document.getElementById("nav ul");
+const nav = document.querySelector("nav");
 
+const navLink = document.querySelectorAll(".nav-link");
 
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+burger.addEventListener('click', () => {
+  ul.classList.toggle('show');
+});
+
+navLink.forEach((link) =>
+  link.addEventListener('click', () => {
+    ul.classList.remove('show');
+  })
+);
